5df4df0598fe0b4590071bbe1007878a633fd7f1
Alexander-Puv <sansano4sana@gmail.com>
2025-06-23 15:34:02 +0300

    add note form

---
+++ app/dashboard/components/DashboardForm.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { addTodo } from "@/lib/actions/addTodo";
+import * as Form from "@radix-ui/react-form";
+import { useState } from "react";
+
+const DashboardForm = () => {
+  const [text, setText] = useState("")
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    const res = await addTodo(text)
+
+    if (res.error) {
+      console.error(res.error)
+    }
+  }
+
+  return (
+    <Form.Root onSubmit={handleSubmit} className="max-w-xl grid gap-5">
+      <Form.Field className="grid" name="text">
+        <div className="flex items-baseline justify-between">
+          <Form.Label>Todo text</Form.Label>
+          <Form.Message className="text-sm opacity-80" match="valueMissing">
+            Please enter the todo text
+          </Form.Message>
+        </div>
+        <Form.Control asChild>
+          <input
+            className="h-9 w-full rounded px-2.5 text-base leading-none"
+            required
+            onChange={e => setText(e.target.value)}
+          />
+        </Form.Control>
+      </Form.Field>
+      <Form.Submit asChild>
+        <Button className="ml-auto">Add todo</Button>
+      </Form.Submit>
+    </Form.Root>
+  )
+}
+
+export default DashboardForm
