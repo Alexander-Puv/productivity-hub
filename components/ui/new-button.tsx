8eed78fbb9ea27578e2e05433ac842d7eddedd2e
Alexander-Puv <sansano4sana@gmail.com>
2025-07-16 22:19:08 +0300

    new button animation and sizing

--- components/ui/new-button.tsx
+++ components/ui/new-button.tsx
@@ -1,10 +1,23 @@
 import { Plus } from 'lucide-react'
 import React from 'react'
 
-const NewButton = ({onClick}: {onClick?: Function}) => {
+interface NewButtonProps {
+  onClick?: Function
+  width?: number
+  height?: number
+}
+
+const NewButton = ({onClick, width = 16, height = 16}: NewButtonProps) => {
   return (
-    <button className="self-center m-1 p-1.5 bg-border rounded-full" onClick={() => onClick && onClick()}>
-      <Plus width={16} height={16} />
+    <button
+      className="
+        self-center m-1 p-1.5 bg-border rounded-[50%]
+        transition-all ease-linear hover:rounded-md hover:bg-primary hover:text-primary-foreground
+        group-hover:rounded-md group-hover:bg-primary group-hover:text-primary-foreground
+      "
+      onClick={() => onClick && onClick()}
+    >
+      <Plus width={width} height={height} />
     </button>
   )
 }
