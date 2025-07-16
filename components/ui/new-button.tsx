adb37145a564734de966fb57267dab78796f7692
Alexander-Puv <sansano4sana@gmail.com>
2025-07-16 21:53:24 +0300

    new button animation

--- components/ui/new-button.tsx
+++ components/ui/new-button.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 
 const NewButton = ({onClick}: {onClick?: Function}) => {
   return (
-    <button className="self-center m-1 p-1.5 bg-border rounded-full" onClick={() => onClick && onClick()}>
+    <button
+      className="
+        self-center m-1 p-1.5 bg-border rounded-[50%]
+        transition-all ease-linear hover:rounded-md hover:bg-primary hover:text-primary-foreground
+      "
+      onClick={() => onClick && onClick()}
+    >
       <Plus width={16} height={16} />
     </button>
   )
