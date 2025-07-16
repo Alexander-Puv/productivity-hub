336859c0c42d49b78e1d59b811132a1a40620f43
Alexander-Puv <sansano4sana@gmail.com>
2025-07-16 22:22:16 +0300

    new button animation

--- components/ui/new-button.tsx
+++ components/ui/new-button.tsx
@@ -13,18 +13,11 @@ const NewButton = ({onClick, width = 16, height = 16}: NewButtonProps) => {
       className="
         self-center m-1 p-1.5 bg-border rounded-[50%]
         transition-all ease-linear hover:rounded-md hover:bg-primary hover:text-primary-foreground
-<<<<<<< HEAD
         group-hover:rounded-md group-hover:bg-primary group-hover:text-primary-foreground
       "
       onClick={() => onClick && onClick()}
     >
       <Plus width={width} height={height} />
-=======
-      "
-      onClick={() => onClick && onClick()}
-    >
-      <Plus width={16} height={16} />
->>>>>>> adb37145a564734de966fb57267dab78796f7692
     </button>
   )
 }
